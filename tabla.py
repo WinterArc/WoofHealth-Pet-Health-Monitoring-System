21f70e5e80e3d59bbbc2beead0d999148b2ff140
WinterArc <89626190+WinterArc@users.noreply.github.com>
2023-04-16 22:15:46 +0800

    Delete tabla.py

--- tabla.py
+++
@@ -1,9 +0,0 @@
-import TableIt
-
-myList = [
-    ["", "a", "b"],
-    ["x", "a + x", "a + b"],
-    ["z", "a + z", "z + b"]
-]
-
-TableIt.printTable(myList, useFieldNames=True, color=(26, 156, 171))
